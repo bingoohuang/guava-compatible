cd0d4107d33494ab81034651247f68fcc41100d7
bingoohuang <bingoo.huang@gmail.com>
2017-02-08 17:35:00 +0800

    comment out TypeToken fix

--- src/test/java/com/google/common/reflect/TypeTokenFix.java
+++ src/test/java/com/google/common/reflect/TypeTokenFix.java
@@ -4,7 +4,7 @@ package com.google.common.reflect;
  * @author bingoohuang [bingoohuang@gmail.com] Created on 2017/2/7.
  */
 public abstract class TypeTokenFix<T> extends TypeToken<T> {
-    public final boolean isAssignableFrom(TypeToken<T> type) {
-        return type.isSubtypeOf(this.getType());
-    }
+//    public final boolean isAssignableFrom(TypeToken<T> type) {
+//        return type.isSubtypeOf(this.getType());
+//    }
 }
